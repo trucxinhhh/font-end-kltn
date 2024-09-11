b307fc558606030db03000163e330b5fccd2ee2e
root <root@ip-172-31-19-111.ap-southeast-1.compute.internal>
2024-09-11 09:53:17 +0000

    Update Image Icon

--- src/Provider.jsx
+++ src/Provider.jsx
@@ -1,3 +1,3 @@
-export const url_api  = 'http://13.251.242.98:1506/'
-export const url_data = 'http://13.251.242.98:2002/'
-export const url_local= 'http://18.140.140.5:5173/'
+export const url_api  = 'http://18.143.27.132:1506/'
+export const url_data = 'http://18.143.27.132:2002/'
+export const url_local= 'http://18.138.48.98:5173/'
